09fcf0dc44cd092175c8a5bde070046b2a0c7a56
jupelaz <jupelaz@gmail.com>
2024-01-08 15:08:57 +0100

    day 3 completed!

--- src/Day03.kt
+++ src/Day03.kt
@@ -34,13 +34,47 @@ fun main() {
 
 
 
-    fun part2(input: List<String>) : Int {
-        return input.size
+  fun part2(input: List<String>) : Int {
+
+    val set = mutableSetOf<Pair<Int, Int>>()
+    val ns = mutableListOf<Int>()
+    input.forEachIndexed { listIndex, line ->
+      line.forEachIndexed { stringIndex, c ->
+        if (c == '*') {
+          set.clear()
+          for (dr in listIndex - 1..listIndex + 1)
+            for (dc in stringIndex - 1..stringIndex + 1)
+              if (dr >= 0 && dr <= input.lastIndex && dc >= 0 && dc <= input.first().lastIndex && input[dr][dc].isDigit()) {
+                var rc = dc
+                while (rc > 0 && input[dr][rc - 1].isDigit())
+                  rc -= 1
+                set.add(dr to rc)
+              }
+          if (set.size == 2) {
+            var mult = 1
+            for (a in set) {
+              val r = a.first
+              var cl = a.second
+              var s = ""
+              while ((cl < input[r].length) && input[r][cl].isDigit()) {
+                s += input[r][cl]
+                cl++
+              }
+              mult *= s.toInt()
+            }
+            ns.add(mult)
+          }
+        }
+      }
     }
+
+    return ns.sum()
+  }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
